c3de4d518b6fa0cd98ebf376bd09ce976c148613
ashwinmothilal <ashwin.mothilal2124@gmail.com>
2020-09-04 23:37:03 +0530

    Rendering secondary variant on the Button.stories.js.

--- scripts/storybook/stories/Button.stories.js
+++ scripts/storybook/stories/Button.stories.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react-native';
 
-import Button from '../../components/Button';
+import Button, {BUTTON_TYPES} from '../../components/Button';
 
 import {View} from 'react-native';
 
 const buttonStories = storiesOf('Buttons', module);
 
-buttonStories.add('Default', () => (
+buttonStories.add(BUTTON_TYPES.PRIMARY, () => (
   <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
     <Button />
+  </View>
+));
+
+buttonStories.add(BUTTON_TYPES.SECONDARY, () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button type={BUTTON_TYPES.SECONDARY} />
   </View>
 ));
