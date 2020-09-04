27b2a01dc179719e7061216e0412205c05b11406
ashwinmothilal <ashwin.mothilal2124@gmail.com>
2020-09-04 23:36:37 +0530

    Installing RNGestureHandler & prop-types. Adding types for Buttons. Using RectButton on Buttons. Adding a theme file named light.js. Moving styles to Stylesheet on Button.js.

---
+++ scripts/Themes/light.js
@@ -0,0 +1,3 @@
+export const PRIMARY_COLOR = '#557571';
+export const SECONDARY_COLOR = '#d49a89';
+export const TEXT_COLOR = '#ffffff';
--- scripts/components/Button.js
+++ scripts/components/Button.js
@@ -1,27 +1,53 @@
-import React, {Component} from 'react';
-import {View, Text, Pressable} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+import {PRIMARY_COLOR, SECONDARY_COLOR, TEXT_COLOR} from '../Themes/light';
+import propTypes from 'prop-types';
 
-class Button extends Component {
-  render() {
-    return (
-      <Pressable
-        onPress={() => {
-          alert('Tset');
-        }}
-        style={{
-          height: 37,
-          width: 150,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#e3e3e3',
-          borderRadius: 5,
-        }}>
-        <Text style={{color: 'black', fontWeight: '500'}}>
-          This is a button
-        </Text>
-      </Pressable>
-    );
-  }
-}
+const BUTTON_TYPES = {
+  PRIMARY: 'Primary',
+  SECONDARY: 'Secondary',
+};
+
+const Button = ({type, text, onPress}) => {
+  return (
+    <RectButton
+      onPress={onPress}
+      style={[
+        ButtonStyles.container,
+        type === BUTTON_TYPES.PRIMARY
+          ? {backgroundColor: PRIMARY_COLOR}
+          : {backgroundColor: SECONDARY_COLOR},
+      ]}>
+      <Text style={ButtonStyles.textStyles}>{text}</Text>
+    </RectButton>
+  );
+};
+
+Button.propTypes = {
+  type: propTypes.string,
+  text: propTypes.string,
+  onPress: propTypes.func,
+};
+
+Button.defaultProps = {
+  type: BUTTON_TYPES.PRIMARY,
+  text: "I'm a Button",
+};
+
+const ButtonStyles = StyleSheet.create({
+  container: {
+    height: 37,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  textStyles: {
+    color: TEXT_COLOR,
+    fontWeight: '500',
+  },
+});
 
 export default Button;
+export {BUTTON_TYPES};
